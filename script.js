a04c13082183eda14e5359a704e95ee6ea0adf12
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-03 14:51:52 +0100

    Add Human Code

---
+++ script.js
@@ -0,0 +1,12 @@
+for(let numbers = 1; numbers <= 100; numbers++){
+    console.log(numbers)
+    if(numbers % 3 == 0) {
+        console.log(!numbers, 'Fizz')
+    }
+
+    if(numbers % 5 == 0){
+        console.log('Buzz');
+        
+    }
+}
+
